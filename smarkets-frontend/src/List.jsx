1d6939b62ee437e06b65e4dab54eb03267a217da
James Yen <jameshyen@gmail.com>
2018-04-24 16:14:02 -0700

    ESLint being picky

--- smarkets-frontend/src/List.jsx
+++ smarkets-frontend/src/List.jsx
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 
 class List extends Component {
   componentDidMount() {
-    fetch(`https://cors-anywhere.herokuapp.com/https://smarkets.com/v0/listings/slug/sport/horse-racing/?period=upcoming`)
-      .then((response) => {
-        return response.json();
-      }).then((json) => {
+    fetch('https://cors-anywhere.herokuapp.com/https://smarkets.com/v0/listings/slug/sport/horse-racing/?period=upcoming')
+      .then(response => response.json())
+      .then((json) => {
         console.log(json);
       });
   }
