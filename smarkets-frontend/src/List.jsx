10dff46d3e49cc205a3682831ae54c31cfc5488e
James Yen <jameshyen@gmail.com>
2018-04-24 16:09:36 -0700

    Hit the API

--- smarkets-frontend/src/List.jsx
+++ smarkets-frontend/src/List.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class List extends Component {
+  componentDidMount() {
+    fetch(`https://cors-anywhere.herokuapp.com/https://smarkets.com/v0/listings/slug/sport/horse-racing/?period=upcoming`)
+      .then((response) => {
+        return response.json();
+      }).then((json) => {
+        console.log(json);
+      });
+  }
+
   render() {
     return (
       <div>Hello.</div>
