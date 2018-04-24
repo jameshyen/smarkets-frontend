0c93b8e59c502db982aee2077ed585e7bc8bd025
James Yen <jameshyen@gmail.com>
2018-04-24 16:25:34 -0700

    It's a start

--- smarkets-frontend/src/List.jsx
+++ smarkets-frontend/src/List.jsx
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 
+const noCors = 'https://cors-anywhere.herokuapp.com/';
+const popularEvents = 'https://fe-api.smarkets.com/v0/events/popular/';
+
 class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+    };
+  }
+
   componentDidMount() {
-    fetch('https://cors-anywhere.herokuapp.com/https://smarkets.com/v0/listings/slug/sport/horse-racing/?period=upcoming')
+    fetch(`${noCors}${popularEvents}`)
       .then(response => response.json())
       .then((json) => {
-        console.log(json);
+        const { results } = json;
+        this.setState({
+          events: results,
+        });
       });
   }
 
   render() {
     return (
-      <div>Hello.</div>
+      <div>
+        <ul>
+          {this.state.events.map((event, index) => <li key={index}>{event.name}</li>)}
+        </ul>
+      </div>
     );
   }
 }
---
+++ smarkets-frontend/src/ListEntry.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const ListEntry = (props) => (
+
+);
+
+export default ListEntry;
